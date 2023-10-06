72f4d3e4d7f228caf9e68bc97c1b45bf1fbc5573
Mytchel Hammond <mytchel.hammond@auros.global>
2023-10-06 10:27:21 +0800

    bump

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setuptools.setup(
         # CVE-2023-37276
         'aiohttp>=3.8.5',
         'pantheon @ git+ssh://bitbucket.org/kenetic/pytheon.git@aiohttp-upgrade-3.8.5',
-        'pyutils @ git+ssh://bitbucket.org/kenetic/pyutils.git@ce56a4b5fddfe7ecab73af88e5dac4ec7b61bb02',
+        'pyutils @ git+ssh://bitbucket.org/kenetic/pyutils.git@dfe77c2e7ec789a3b64e2af6b30e7e5e5c05b039',
         # refs/tags/v6.0.0-beta.8
         # require w3 beta 6 to fix dependency conflict with solana/websockets.
         'Web3 @ git+https://github.com/ethereum/web3.py.git@de95191dea8eb56e5176693946fb1e50957b8a5c',
