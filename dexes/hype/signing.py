9be7046f04076ef67d1039d1a01c7a7ab83643db
Chris Duck <chris.duck@auros.global>
2024-05-23 22:23:48 +0000

    Hype fixes

--- dexes/hype/signing.py
+++ dexes/hype/signing.py
@@ -164,6 +164,43 @@ def sign_usd_transfer_action(wallet, message, is_mainnet):
     }
     return sign_inner(wallet, data)
 
+def sign_agent(wallet, action, is_mainnet):
+    return sign_user_signed_action(
+        wallet,
+        action,
+        [
+            {"name": "hyperliquidChain", "type": "string"},
+            {"name": "agentAddress", "type": "address"},
+            {"name": "agentName", "type": "string"},
+            {"name": "nonce", "type": "uint64"},
+        ],
+        "HyperliquidTransaction:ApproveAgent",
+        is_mainnet,
+    )
+
+def sign_user_signed_action(wallet, action, payload_types, primary_type, is_mainnet):
+    action["signatureChainId"] = "0x66eee"
+    action["hyperliquidChain"] = "Mainnet" if is_mainnet else "Testnet"
+    data = {
+        "domain": {
+            "name": "HyperliquidSignTransaction",
+            "version": "1",
+            "chainId": 421614,
+            "verifyingContract": "0x0000000000000000000000000000000000000000",
+        },
+        "types": {
+            primary_type: payload_types,
+            "EIP712Domain": [
+                {"name": "name", "type": "string"},
+                {"name": "version", "type": "string"},
+                {"name": "chainId", "type": "uint256"},
+                {"name": "verifyingContract", "type": "address"},
+            ],
+        },
+        "primaryType": primary_type,
+        "message": action,
+    }
+    return sign_inner(wallet, data)
 
 def sign_withdraw_from_bridge_action(wallet, message, is_mainnet):
     data = {
@@ -188,32 +225,6 @@ def sign_withdraw_from_bridge_action(wallet, message, is_mainnet):
         },
         "primaryType": "WithdrawFromBridge2SignPayload",
         "message": message,
-    }
-    return sign_inner(wallet, data)
-
-
-def sign_agent(wallet, agent, is_mainnet):
-    data = {
-        "domain": {
-            "name": "Exchange",
-            "version": "1",
-            "chainId": 42161 if is_mainnet else 421614,
-            "verifyingContract": "0x0000000000000000000000000000000000000000",
-        },
-        "types": {
-            "Agent": [
-                {"name": "source", "type": "string"},
-                {"name": "connectionId", "type": "bytes32"},
-            ],
-            "EIP712Domain": [
-                {"name": "name", "type": "string"},
-                {"name": "version", "type": "string"},
-                {"name": "chainId", "type": "uint256"},
-                {"name": "verifyingContract", "type": "address"},
-            ],
-        },
-        "primaryType": "Agent",
-        "message": agent,
     }
     return sign_inner(wallet, data)
 
