4dc6ef7477bc2fe06cc9e86ccd00e81115d52a8b
Yahai Wang <yahai.wang@auros.global>
2024-07-08 10:13:01 +0000

    PER dex proxy

--- setup.py
+++ setup.py
@@ -20,6 +20,6 @@ setuptools.setup(
     py_modules=[],
     install_requires=[
         'msgpack==1.0.8',
-        "pyutils @ git+ssh://git@bitbucket.org/kenetic/pyutils.git@pyutils-1.11.23"
+        "pyutils @ git+ssh://git@bitbucket.org/kenetic/pyutils.git@pyutils-1.11.24"
     ]
 )
