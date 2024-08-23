327f363dadd38ff79108710ac4be630a6002999b
Nitul Datt <nitul.datt@auros.global>
2024-08-23 15:00:36 +0530

    [deep]: Fix bug with order querying

--- ts/dexes/deepbook/test/test_get_order_request.ts
+++ ts/dexes/deepbook/test/test_get_order_request.ts
@@ -4,7 +4,7 @@ const main = async () => {
     const poolId = "0x4405b50d791fd3346754e8171aaab6bc2ed26c2c46efdd033c14b30ae507ac33";
     const clientOrderId = "1";
 
-    await get("orders", { "pool_id": poolId, "client_order_id": clientOrderId });
+    await get("order", { "pool_id": poolId, "client_order_id": clientOrderId });
 }
 
 await main()
