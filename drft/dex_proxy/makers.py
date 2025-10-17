582e7b7226d0fc6c0e2d7536b06ed295e8e9aa2c
Raviprakash Chandak <raviprakash.chandak@auros.global>
2025-10-17 11:22:10 +0000

    [drft] Dex proxy drift add support for subaccount based REST trades fetching

--- drft/dex_proxy/makers.py
+++ drft/dex_proxy/makers.py
@@ -3,8 +3,8 @@ import logging
 from decimal import Decimal
 from typing import Dict
 
-from .drift_api import MarketType
-from .clients_pool import ClientsPool
+from drift_api import MarketType
+from clients_pool import ClientsPool
 
 from solders.pubkey import Pubkey
 from solana.rpc.commitment import Confirmed
